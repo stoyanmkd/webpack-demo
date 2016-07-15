50cd502660d37bd34440a7fd25ca6c237094e439
Stoyan Makedonski <makedonski@ifao.net>
2016-07-15 11:35:34 +0300

    Updates

--- app.5da505ada2365030215b.js
+++ app.5da505ada2365030215b.js
@@ -1,2 +1,2 @@
 webpackJsonp([0,3],[function(e,n,t){var o=t(1);t(2);document.body.appendChild(o())},function(e,n){e.exports=function(){function e(){t.innerHTML=c+"",console.log(c),c++}var n=document.createElement("h1"),t=document.createElement("h2"),o=document.createElement("div"),c=0;return n.innerHTML="Counter: ",e(),setInterval(function(){e()},1e3),o.appendChild(n),o.appendChild(t),o}}]);
-//# sourceMappingURL=app.04b06e55c49b5fc51259.js.map
+//# sourceMappingURL=app.5da505ada2365030215b.js.map
---
+++ style.5da505ada2365030215b.js
@@ -0,0 +1,2 @@
+webpackJsonp([1,3],[function(n,c){}]);
+//# sourceMappingURL=style.5da505ada2365030215b.js.map
